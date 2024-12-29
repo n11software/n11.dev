794cebc1659af0766d1238a2f5b120afd974637f
Levi Hicks <levih@Levis-MacBook-Pro.local>
2024-12-29 09:08:12 -0600

    a little goofy but it'll do
    Signed-off-by: Levi Hicks <levih@Levis-MacBook-Pro.local>

---
+++ utils/Install.sh
@@ -0,0 +1,19 @@
+sudo apt update;sudo apt upgrade -y;sudo apt install -y git nodejs npm;
+
+NODE=$(which node)
+
+# Create N11 user
+useradd -m n11
+
+# clone the repo
+git clone https://github.com/N11Software/n11.dev /home/n11/n11.dev
+
+setcap 'cap_net_bind_service=+ep' $NODE
+
+# Install the dependencies
+cd /home/n11/n11.dev
+npm install
+
+# move the service file
+cp /home/n11/n11.dev/utils/N11.service /etc/systemd/system/n11.service
+systemctl enable --now n11
