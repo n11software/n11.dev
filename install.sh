b114e6397f4ebf9952ee7c14c2cdd9984309b451
Levi Hicks <levih@n11.dev>
2025-02-08 00:02:39 -0600

    Possible fix for liboqs install
    Signed-off-by: Levi Hicks <levih@n11.dev>

--- install.sh
+++ install.sh
@@ -216,7 +216,14 @@ if [ ! -f /usr/lib/liboqs.a ]; then
     if [ "$OS" = "Linux" ]; then
         if [ "$DISTRO" = "Ubuntu" ]; then
             echo "Installing OQS"
-            sudo apt-get install -y liboqs-dev
+            sudo apt install astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind
+            git clone -b main https://github.com/open-quantum-safe/liboqs.git
+            cd liboqs
+            mkdir build && cd build
+            cmake -GNinja ..
+            ninja
+            cd ../../
+            rm -r liboqs
         elif [ "$DISTRO" = "Fedora" ]; then
             echo "Installing OQS"
             sudo dnf install -y oqs-devel
